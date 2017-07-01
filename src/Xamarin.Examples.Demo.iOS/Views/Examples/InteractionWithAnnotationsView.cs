cb6c7c601de3e27027ef0dec531609024f9191a6
SciChart DevTeam <devteam@scichart.com>
2017-07-01 09:42:58 +0000

    SciChart version 2.0.0.495
    Revision Message
    -------- -------
         455 - Added bindings for SCIDataSeriesPbserver and SCICollectionObserver
             as well as all related such as SCIDataSeriesProtocol etc???
             - Added isHidden property to AnnotationBase binding
             - updated horizontal\vertical line annotation bindings
             - Fixed the issue with CustomAnnotation which was related to wrong
             bindings.
         456 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         457 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         458 Renamed SCIXYDirection -> SCIDirection2D on xamarin.ios side
         459 Removed redundant comment
         461 Fix the broken build
         462 removed RecyclerView dependency from Xamarin.Android;
         470 Fixed broken UI Tests: added WaitForElement method which is looking
             for SCIOpenGlRenderSurface to appear, so that we can all the
             stuff(from scichart) is rendered; Removed Assign attribute from
             CustomAnnotation class binding - since this is used for value types
             only
         483 Merged revision(s) 460-482 from
             branches/SCIGenericType_feasibility_brunch:
    
         485 Merged revision(s) 483-484 from
             branches/SCIGenericType_feasibility_brunch:
    
         486 Changed base type in DateValuesFactory to be SCIDataType.DateTime
             instead of Double to fix RealtimeTickingStockChart issue
         487 Added bindings for SCIUpdateSuspender and related classes\protocols,
             updated surface, axis, annotations bindings due to update with
             suspendable, and in case of annotations with adding isSelected
             property.
         488 Updated TODO comment
         490 Updated bindings after changes to UniformHeatmap, which finished
             extras for all data series
         491 Updated all examples with suspend updates, also some small cosmetic
             changes to both iOS and android
         492 Build script for source zip
         493 Added bindings related to ThemeManager
             Updated Annotation examples in xamarin.ios
    
         494 Updated horizontal/vertical line annotation due to changes in
             scichart ios framework
         495 SDK zip and git fixes

---
+++ src/Xamarin.Examples.Demo.iOS/Views/Examples/InteractionWithAnnotationsView.cs
@@ -0,0 +1,264 @@
+﻿using SciChart.Examples.Demo.Data;
+using SciChart.iOS.Charting;
+using UIKit;
+using Xamarin.Examples.Demo.iOS.Resources.Layout;
+using Xamarin.Examples.Demo.iOS.Views.Base;
+using SciChart.Examples.Demo.Fragments.Base;
+using Xamarin.Examples.Demo.Utils;
+using System;
+using Xamarin.Examples.Demo.Data;
+using System.Linq;
+
+namespace Xamarin.Examples.Demo.iOS.Views.Examples
+{
+    [ExampleDefinition("Interaction with Annotations", description: "Demonstrates touch-interaction with Annotations", icon: ExampleIcon.Annotations)]
+    public class InteractionWithAnnotationsView : ExampleBaseView<SingleChartViewLayout>
+    {
+        private readonly SingleChartViewLayout _exampleViewLayout = SingleChartViewLayout.Create();
+        public override SingleChartViewLayout ExampleViewLayout => _exampleViewLayout;
+
+        public SCIChartSurface Surface => ExampleViewLayout.SciChartSurface;
+
+        protected override void UpdateFrame()
+        {
+            Surface.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            NSLayoutConstraint constraintRight = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 1, 0);
+            NSLayoutConstraint constraintLeft = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Left, 1, 0);
+            NSLayoutConstraint constraintTop = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Top, NSLayoutRelation.Equal, this, NSLayoutAttribute.Top, 1, 0);
+            NSLayoutConstraint constraintBottom = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 1, 0);
+
+            this.AddConstraint(constraintRight);
+            this.AddConstraint(constraintLeft);
+            this.AddConstraint(constraintTop);
+            this.AddConstraint(constraintBottom);
+        }
+
+        protected override void InitExampleInternal()
+        {
+            var dataSeries = new OhlcDataSeries<DateTime, double>();
+
+            var marketDataService = new MarketDataService(new DateTime(2000, 08, 01, 12, 00, 00), 5, 5);
+            var data = marketDataService.GetHistoricalData(200);
+
+            dataSeries.Append(data.Select(x => x.DateTime), data.Select(x => x.Open), data.Select(x => x.High), data.Select(x => x.Low), data.Select(x => x.Close));
+
+            Surface.XAxes.Add(new SCICategoryDateTimeAxis());
+            Surface.YAxes.Add(new SCINumericAxis { VisibleRange = new SCIDoubleRange(30, 37) });
+            Surface.RenderableSeries.Add(new SCIFastCandlestickRenderableSeries { DataSeries = dataSeries });
+            Surface.ChartModifiers = new SCIChartModifierCollection
+            {
+                new SCIZoomPanModifier(),
+                new SCIPinchZoomModifier(),
+                new SCIZoomExtentsModifier()
+            };
+
+            var horizontalLine1 = new SCIHorizontalLineAnnotation
+            {
+                X1Value = 150,
+                Y1Value = 32.2,
+                HorizontalAlignment = SCIHorizontalLineAnnotationAlignment.Right,
+                Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.Red, 2f) },
+                IsEditable = true
+            };
+            horizontalLine1.AddLabel(new SCILineAnnotationLabel
+            {
+                Style =
+                {
+                    TextStyle = new SCITextFormattingStyle {Color = UIColor.White},
+                    LabelPlacement = SCILabelPlacement.Axis,
+                    BackgroundColor = ColorUtil.Red.ToColor()
+                }
+            });
+
+            var horizontalLine2 = new SCIHorizontalLineAnnotation
+            {
+                X1Value = 130,
+                X2Value = 160,
+                Y1Value = 33.9,
+                HorizontalAlignment = SCIHorizontalLineAnnotationAlignment.Center,
+                Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.Blue, 2f) },
+                IsEditable = true,
+            };
+            horizontalLine2.AddLabel(new SCILineAnnotationLabel
+            {
+                Style =
+                {
+                    TextStyle = new SCITextFormattingStyle {Color = UIColor.Blue},
+                    LabelPlacement = SCILabelPlacement.Top,
+                    BackgroundColor = UIColor.Clear
+                },
+                Text = "Top",
+            });
+            horizontalLine2.AddLabel(new SCILineAnnotationLabel
+            {
+                Style =
+                {
+                    TextStyle = new SCITextFormattingStyle {Color = UIColor.Blue},
+                    LabelPlacement = SCILabelPlacement.Left,
+                    BackgroundColor = UIColor.Clear
+                },
+                Text = "Left",
+            });
+            horizontalLine2.AddLabel(new SCILineAnnotationLabel
+            {
+                Style =
+                {
+                    TextStyle = new SCITextFormattingStyle {Color = UIColor.Blue},
+                    LabelPlacement = SCILabelPlacement.Right,
+                    BackgroundColor = UIColor.Clear
+                },
+                Text = "Right",
+            });
+
+            var verticalLine1 = new SCIVerticalLineAnnotation
+            {
+                X1Value = 40,
+                Y1Value = 34,
+                VerticalAlignment = SCIVerticalLineAnnotationAlignment.Top,
+                Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.Green, 2f) },
+                IsEditable = true,
+            };
+            verticalLine1.AddLabel(new SCILineAnnotationLabel
+            {
+                Style =
+                {
+                    TextStyle = new SCITextFormattingStyle {Color = UIColor.Green},
+                    LabelPlacement = SCILabelPlacement.Top,
+                    BackgroundColor = UIColor.Clear
+                },
+            });
+
+            Surface.Annotations = new SCIAnnotationCollection
+            {
+                new SCITextAnnotation
+                {
+                    CoordinateMode = SCIAnnotationCoordinateMode.Relative,
+                    HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Center,
+                    Text = "EUR.USD",
+                    X1Value = 0.5,
+                    Y1Value = 0.5,
+                    Style = new SCITextAnnotationStyle
+                    {
+                        TextColor = 0x77FFFFFF.ToColor(),
+                        TextStyle = new SCITextFormattingStyle {FontSize = 72 },
+                        BackgroundColor = UIColor.Clear
+                    },
+                },
+                new SCITextAnnotation
+                {
+                    CoordinateMode = SCIAnnotationCoordinateMode.Absolute,
+                    IsEditable = true,
+                    EditableText = false,
+					Text = "Buy!",
+                    X1Value = 10d,
+                    Y1Value = 30.5d,
+                    VerticalAnchorPoint = SCIVerticalAnchorPoint.Bottom,
+                    HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Left,
+                    Style =
+                    {
+                        TextStyle = { FontSize = 20 },
+                        TextColor = ColorUtil.White.ToColor(),
+                        BackgroundColor = UIColor.Clear
+                    },
+                },
+                new SCITextAnnotation
+                {
+                    CoordinateMode = SCIAnnotationCoordinateMode.Absolute,
+                    IsEditable = true,
+                    EditableText = false,
+                    Text = "Sell!",
+                    X1Value = 50d,
+                    Y1Value = 34d,
+                    Style =
+                    {
+                        TextStyle = { FontSize = 20 },
+                        TextColor = ColorUtil.White.ToColor(),
+                        BackgroundColor = 0x33FF3333.ToColor()
+                    },
+                },
+                new SCIBoxAnnotation
+                {
+                    IsEditable = true,
+                    Style =
+                    {
+                        FillBrush = new SCISolidBrushStyle(UIColor.FromRGBA(0xFF, 0x66, 0x00, 0x33)),
+                        BorderPen =  new SCISolidPenStyle(UIColor.FromRGBA(0xFF, 0x66, 0x00, 0x77), 1.0f)
+                    },
+                    X1Value = 50,
+                    Y1Value = 35.5,
+                    X2Value = 120,
+                    Y2Value = 32,
+                },
+                new SCILineAnnotation
+                {
+                    IsEditable = true,
+                    Style = { LinePen = new SCISolidPenStyle(0xAAFF6600, 2f) },
+                    X1Value = 40,
+                    Y1Value = 30.5,
+                    X2Value = 60,
+                    Y2Value = 33.5,
+                },
+                new SCILineAnnotation
+                {
+                    IsEditable = true,
+                    Style = { LinePen = new SCISolidPenStyle(0xAAFF6600, 2f) },
+                    X1Value = 120,
+                    Y1Value = 30.5,
+                    X2Value = 175,
+                    Y2Value = 36,
+                },
+                // TODO LineArrow annotaiton
+                new SCILineAnnotation
+                {
+                    IsEditable = true,
+                    Style = { LinePen = new SCISolidPenStyle(0xAAFF6600, 2f) },
+                    X1Value = 50,
+                    Y1Value = 35,
+                    X2Value = 80,
+                    Y2Value = 31.4,
+                },
+                new SCIAxisMarkerAnnotation
+                {
+                    CoordinateMode = SCIAnnotationCoordinateMode.Absolute,
+                    IsEditable = true,
+                    Position = 32.7,
+                },
+                new SCIAxisMarkerAnnotation
+                {
+                    FormattedValue = "Horizontal",
+                    IsEditable = true,
+                    Position = 100,
+                },
+
+                horizontalLine1,
+                horizontalLine2,
+                new SCIVerticalLineAnnotation
+                {
+                    X1Value = 20,
+                    Y1Value = 35,
+                    Y2Value = 33,
+                    VerticalAlignment = SCIVerticalLineAnnotationAlignment.Center,
+                    Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.DarkGreen, 2f) },
+                    IsEditable = true,
+                },
+                verticalLine1,
+                new SCITextAnnotation
+                {
+                    X1Value = 50,
+                    Y1Value = 37,
+                    IsEditable = true,
+                    EditableText = false,
+					Text = "Rotated text",
+                    Style =
+                    {
+                        TextStyle = { FontSize = 20 },
+                        TextColor = ColorUtil.White.ToColor(),
+                        BackgroundColor = UIColor.Clear
+                    },
+                    //RotationAngle = 30
+                }
+            };
+        }
+    }
+}
---
+++ src/Xamarin.Examples.Demo.iOS/Views/Examples/StackedMountainChartView.cs
@@ -0,0 +1,75 @@
+﻿using SciChart.Examples.Demo.Fragments.Base;
+using SciChart.iOS.Charting;
+using UIKit;
+using Xamarin.Examples.Demo.iOS.Resources.Layout;
+using Xamarin.Examples.Demo.iOS.Views.Base;
+
+namespace Xamarin.Examples.Demo.iOS.Views.Examples
+{
+    [ExampleDefinition("Stacked Mountain Chart", description: "Demonstrates a Stacked Mountain Chart", icon: ExampleIcon.StackedMountainChart)]
+    public class StackedMountainChartView : ExampleBaseView<SingleChartViewLayout>
+    {
+        private readonly SingleChartViewLayout _exampleViewLayout = SingleChartViewLayout.Create();
+        public override SingleChartViewLayout ExampleViewLayout => _exampleViewLayout;
+
+        public SCIChartSurface Surface => ExampleViewLayout.SciChartSurface;
+
+        protected override void UpdateFrame()
+        {
+            Surface.TranslatesAutoresizingMaskIntoConstraints = false;
+
+            NSLayoutConstraint constraintRight = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 1, 0);
+            NSLayoutConstraint constraintLeft = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Left, 1, 0);
+            NSLayoutConstraint constraintTop = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Top, NSLayoutRelation.Equal, this, NSLayoutAttribute.Top, 1, 0);
+            NSLayoutConstraint constraintBottom = NSLayoutConstraint.Create(Surface, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 1, 0);
+
+            this.AddConstraint(constraintRight);
+            this.AddConstraint(constraintLeft);
+            this.AddConstraint(constraintTop);
+            this.AddConstraint(constraintBottom);
+        }
+
+        protected override void InitExampleInternal()
+        {
+            var xAxis = new SCINumericAxis();
+            var yAxis = new SCINumericAxis();
+
+            var yValues1 = new[] { 4.0, 7, 5.2, 9.4, 3.8, 5.1, 7.5, 12.4, 14.6, 8.1, 11.7, 14.4, 16.0, 3.7, 5.1, 6.4, 3.5, 2.5, 12.4, 16.4, 7.1, 8.0, 9.0 };
+            var yValues2 = new[] { 15.0, 10.1, 10.2, 10.4, 10.8, 1.1, 11.5, 3.4, 4.6, 0.1, 1.7, 14.4, 6.0, 13.7, 10.1, 8.4, 8.5, 12.5, 1.4, 0.4, 10.1, 5.0, 1.0 };
+
+            var ds1 = new XyDataSeries<double, double> { SeriesName = "data 1" };
+            var ds2 = new XyDataSeries<double, double> { SeriesName = "data 2" };
+
+            for (var i = 0; i < yValues1.Length; i++) ds1.Append(i, yValues1[i]);
+            for (var i = 0; i < yValues2.Length; i++) ds2.Append(i, yValues2[i]);
+
+            var series1 = GetRenderableSeries(ds1, 0xDDDBE0E1, 0x88B6C1C3);
+            var series2 = GetRenderableSeries(ds2, 0xDDACBCCA, 0x88439AAF);
+
+            var seriesCollection = new SCIVerticallyStackedMountainsCollection();
+            seriesCollection.Add(series1);
+            seriesCollection.Add(series2);
+
+            using (Surface.SuspendUpdates())
+            {
+                Surface.XAxes.Add(xAxis);
+                Surface.YAxes.Add(yAxis);
+                Surface.RenderableSeries.Add(seriesCollection);
+                Surface.ChartModifiers = new SCIChartModifierCollection
+                {
+                new SCICursorModifier(),
+                new SCIZoomExtentsModifier()
+                };
+            }
+        }
+
+        private SCIStackedMountainRenderableSeries GetRenderableSeries(IDataSeries dataSeries, uint fillColorStart, uint fillColorEbd)
+        {
+            return new SCIStackedMountainRenderableSeries
+            {
+                DataSeries = dataSeries,
+                AreaStyle = new SCILinearGradientBrushStyle(fillColorStart, fillColorEbd, SCILinearGradientDirection.Vertical),
+            };
+        }
+    }
+}
