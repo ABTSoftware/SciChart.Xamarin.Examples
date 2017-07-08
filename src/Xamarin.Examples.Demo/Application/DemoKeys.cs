55abff938dc5c2e0a49ecfc0039c09c85154fd21
SciChart DevTeam <devteam@scichart.com>
2017-07-08 10:20:53 +0000

    SciChart version 2.0.0.520

--- src/Xamarin.Examples.Demo/Application/DemoKeys.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SciChart.Examples.Demo.Application
-{
-    public class DemoKeys
-    {
-        public static readonly string ExampleId = "exampleId";
-        public static readonly string FragmentTag = "fragmentTag";
-    }
-}
