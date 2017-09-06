cf564bd012115a2d748f4ac26f246cd004f99bbc
SciChart DevTeam <devteam@scichart.com>
2017-09-06 08:08:40 +0000

    SciChart version 2.0.1.524
    Revision Message
    -------- -------
         455 - Added bindings for SCIDataSeriesPbserver and SCICollectionObserver
             as well as all related such as SCIDataSeriesProtocol etc???
             - Added isHidden property to AnnotationBase binding
             - updated horizontal\vertical line annotation bindings
             - Fixed the issue with CustomAnnotation which was related to wrong
             bindings.
         456 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         457 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         458 Renamed SCIXYDirection -> SCIDirection2D on xamarin.ios side
         459 Removed redundant comment
         461 Fix the broken build
         462 removed RecyclerView dependency from Xamarin.Android;
         470 Fixed broken UI Tests: added WaitForElement method which is looking
             for SCIOpenGlRenderSurface to appear, so that we can all the
             stuff(from scichart) is rendered; Removed Assign attribute from
             CustomAnnotation class binding - since this is used for value types
             only
         483 Merged revision(s) 460-482 from
             branches/SCIGenericType_feasibility_brunch:
    
         485 Merged revision(s) 483-484 from
             branches/SCIGenericType_feasibility_brunch:
    
         486 Changed base type in DateValuesFactory to be SCIDataType.DateTime
             instead of Double to fix RealtimeTickingStockChart issue
         487 Added bindings for SCIUpdateSuspender and related classes\protocols,
             updated surface, axis, annotations bindings due to update with
             suspendable, and in case of annotations with adding isSelected
             property.
         488 Updated TODO comment
         490 Updated bindings after changes to UniformHeatmap, which finished
             extras for all data series
         491 Updated all examples with suspend updates, also some small cosmetic
             changes to both iOS and android
         492 Build script for source zip
         493 Added bindings related to ThemeManager
             Updated Annotation examples in xamarin.ios
    
         494 Updated horizontal/vertical line annotation due to changes in
             scichart ios framework
         495 SDK zip and git fixes
         496 Updated bindings related to r1608 commit (annotationSurface enum, and
             related annotations)
         497 Added horizontal axis marker annotation (scios-548)
         498 Added default theme for interaction with annotation view example
             Removed redundant code from bubble chart exmaple
         499 Added berry blue theme to using theme manager example (SCIOS-502)
         500 Migrated app icons and launch images to xcassets
         501 Reworked xamarin.ios demo app, removed redundant views and lot???s of
             constraints, now examples are view controllers as they should be.
             Fixed issue when performance demo won???t start after you step into
             example, start, back, and then step into and try to start again
             Added label provider to stacked column side by side example, fixed x
             axis by adding minor delta
             Removed ExampleViewController and segue to it, as it???s now redundant
             Removed styling of NavigationBar in code, instead do that in
             storyboard
             Updated .xib layout files with proper views and constraints
             Cleaned up the code.
    
         502 Dummy commit
         503 Removed using of missing icon in table view cell
         504 Should fix the build error in SciChart Xamarin github
         505 Fixed formatting issue in multi pane stock chart in xamarin.ios
         506 Fixed broken RealTimeTickingStock chart view example
         522 Added showcase demo project for Xamarin platform
         524 Removed packages folder, put it into ignored list

---
+++ src/Xamarin.Examples.Demo.iOS/Resources/Layout/UsingThemeManagerLayout.designer.cs
@@ -0,0 +1,38 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace Xamarin.Examples.Demo.iOS.Resources.Layout
+{
+    [Register ("UsingThemeManagerLayout")]
+    partial class UsingThemeManagerLayout
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        SciChart.iOS.Charting.SCIChartSurface Surface { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton ThemeButton { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (Surface != null) {
+                Surface.Dispose ();
+                Surface = null;
+            }
+
+            if (ThemeButton != null) {
+                ThemeButton.Dispose ();
+                ThemeButton = null;
+            }
+        }
+    }
+}
