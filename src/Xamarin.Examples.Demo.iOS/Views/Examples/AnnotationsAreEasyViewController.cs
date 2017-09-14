2350390775866f96aba02c33af934b6e43ab4ba0
SciChart DevTeam <devteam@scichart.com>
2017-09-14 14:02:59 +0000

    SciChart version 2.0.1.526
    Revision Message
    -------- -------
         455 - Added bindings for SCIDataSeriesPbserver and SCICollectionObserver
             as well as all related such as SCIDataSeriesProtocol etc???
             - Added isHidden property to AnnotationBase binding
             - updated horizontal\vertical line annotation bindings
             - Fixed the issue with CustomAnnotation which was related to wrong
             bindings.
         456 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         457 Removed InvalidetElement() calls from examples, not it isn???t needed
              !!!
         458 Renamed SCIXYDirection -> SCIDirection2D on xamarin.ios side
         459 Removed redundant comment
         461 Fix the broken build
         462 removed RecyclerView dependency from Xamarin.Android;
         470 Fixed broken UI Tests: added WaitForElement method which is looking
             for SCIOpenGlRenderSurface to appear, so that we can all the
             stuff(from scichart) is rendered; Removed Assign attribute from
             CustomAnnotation class binding - since this is used for value types
             only
         483 Merged revision(s) 460-482 from
             branches/SCIGenericType_feasibility_brunch:
    
         485 Merged revision(s) 483-484 from
             branches/SCIGenericType_feasibility_brunch:
    
         486 Changed base type in DateValuesFactory to be SCIDataType.DateTime
             instead of Double to fix RealtimeTickingStockChart issue
         487 Added bindings for SCIUpdateSuspender and related classes\protocols,
             updated surface, axis, annotations bindings due to update with
             suspendable, and in case of annotations with adding isSelected
             property.
         488 Updated TODO comment
         490 Updated bindings after changes to UniformHeatmap, which finished
             extras for all data series
         491 Updated all examples with suspend updates, also some small cosmetic
             changes to both iOS and android
         492 Build script for source zip
         493 Added bindings related to ThemeManager
             Updated Annotation examples in xamarin.ios
    
         494 Updated horizontal/vertical line annotation due to changes in
             scichart ios framework
         495 SDK zip and git fixes
         496 Updated bindings related to r1608 commit (annotationSurface enum, and
             related annotations)
         497 Added horizontal axis marker annotation (scios-548)
         498 Added default theme for interaction with annotation view example
             Removed redundant code from bubble chart exmaple
         499 Added berry blue theme to using theme manager example (SCIOS-502)
         500 Migrated app icons and launch images to xcassets
         501 Reworked xamarin.ios demo app, removed redundant views and lot???s of
             constraints, now examples are view controllers as they should be.
             Fixed issue when performance demo won???t start after you step into
             example, start, back, and then step into and try to start again
             Added label provider to stacked column side by side example, fixed x
             axis by adding minor delta
             Removed ExampleViewController and segue to it, as it???s now redundant
             Removed styling of NavigationBar in code, instead do that in
             storyboard
             Updated .xib layout files with proper views and constraints
             Cleaned up the code.
    
         502 Dummy commit
         503 Removed using of missing icon in table view cell
         504 Should fix the build error in SciChart Xamarin github
         505 Fixed formatting issue in multi pane stock chart in xamarin.ios
         506 Fixed broken RealTimeTickingStock chart view example
         522 Added showcase demo project for Xamarin platform
         524 Removed packages folder, put it into ignored list
         525 SCIOS-574 Added IntroPage, ExampleListPage; Added basic
             implementation for CustomRenderer for scichart surface

---
+++ src/Xamarin.Examples.Demo.iOS/Views/Examples/AnnotationsAreEasyViewController.cs
@@ -0,0 +1,287 @@
+﻿using UIKit;
+using SciChart.Examples.Demo.Fragments.Base;
+using Xamarin.Examples.Demo.iOS.Views.Base;
+using Xamarin.Examples.Demo.iOS.Resources.Layout;
+using SciChart.iOS.Charting;
+using Xamarin.Examples.Demo.Utils;
+using System;
+
+namespace Xamarin.Examples.Demo.iOS
+{
+    [ExampleDefinition("Annotations are Easy", description: "Demonstrates how to use Annotations", icon: ExampleIcon.Annotations)]
+    public class AnnotationsAreEasyViewController : ExampleBaseViewController
+    {
+        public override Type ExampleViewType => typeof(SingleChartViewLayout);
+
+        public SCIChartSurface Surface => ((SingleChartViewLayout)View).SciChartSurface;
+
+        protected override void InitExample()
+        {
+            using (Surface.SuspendUpdates())
+            {
+                Surface.XAxes.Add(new SCINumericAxis { GrowBy = new SCIDoubleRange(0.1, 0.1), VisibleRange = new SCIDoubleRange(0.0, 10.0) });
+                Surface.YAxes.Add(new SCINumericAxis { GrowBy = new SCIDoubleRange(0.1, 0.1), VisibleRange = new SCIDoubleRange(0.0, 10.0) });
+
+                var horizontalLine1 = new SCIHorizontalLineAnnotation
+                {
+                    X1Value = 5.0,
+                    Y1Value = 3.2,
+                    HorizontalAlignment = SCIHorizontalLineAnnotationAlignment.Right,
+                    Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(UIColor.Orange, 2f) },
+                };
+                horizontalLine1.AddLabel(new SCILineAnnotationLabel
+                {
+                    Text = "Right Aligned, with text on left",
+                    Style =
+                    {
+                        TextStyle = new SCITextFormattingStyle {Color = UIColor.Orange},
+                        LabelPlacement = SCILabelPlacement.TopLeft,
+                        BackgroundColor = UIColor.Clear
+                    }
+                });
+
+                var horizontalLine2 = new SCIHorizontalLineAnnotation
+                {
+                    X1Value = 7.0,
+                    Y1Value = 2.8,
+                    HorizontalAlignment = SCIHorizontalLineAnnotationAlignment.Stretch,
+                    Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(UIColor.Orange, 2f) }
+                };
+                horizontalLine2.AddLabel(new SCILineAnnotationLabel
+                {
+                    Style =
+                    {
+                        TextStyle = new SCITextFormattingStyle {Color = UIColor.Black},
+                        LabelPlacement = SCILabelPlacement.Axis,
+                        BackgroundColor = UIColor.Orange
+                    }
+                });
+
+                var verticalLine1 = new SCIVerticalLineAnnotation
+                {
+                    X1Value = 9.0,
+                    Y1Value = 4.0,
+                    VerticalAlignment = SCIVerticalLineAnnotationAlignment.Bottom,
+                    Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.Brown.ToColor(), 2f) }
+                };
+                verticalLine1.AddLabel(new SCILineAnnotationLabel
+                {
+                    TextColor = UIColor.White,
+                    Style =
+                    {
+                        LabelPlacement = SCILabelPlacement.Axis,
+                        BackgroundColor = ColorUtil.Brown.ToColor()
+                    }
+                });
+
+                var verticalLine2 = new SCIVerticalLineAnnotation
+                {
+                    X1Value = 9.5,
+                    Y1Value = 3.0,
+                    VerticalAlignment = SCIVerticalLineAnnotationAlignment.Stretch,
+                    Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(ColorUtil.Brown.ToColor(), 2f) }
+                };
+                verticalLine2.AddLabel(new SCILineAnnotationLabel
+                {
+                    TextColor = UIColor.White,
+                    Style =
+                    {
+                        LabelPlacement = SCILabelPlacement.Axis,
+                        BackgroundColor = ColorUtil.Brown.ToColor()
+                    }
+                });
+                verticalLine2.AddLabel(new SCILineAnnotationLabel
+                {
+                    Text = "Bottom-aligned",
+                    Style =
+                    {
+                        TextStyle = new SCITextFormattingStyle {Color = ColorUtil.Brown.ToColor()},
+                        LabelPlacement = SCILabelPlacement.TopRight,
+                        BackgroundColor = UIColor.Clear
+                    }
+                });
+
+                Surface.Annotations = new SCIAnnotationCollection
+                {
+                    // Watermark
+                    new SCITextAnnotation
+                    {
+                        X1Value = 0.5,
+                        Y1Value = 0.5,
+                        Text = "Create \nWatermarks",
+                        Style = new SCITextAnnotationStyle
+                        {
+                            TextColor = 0x22FFFFFF.ToColor(),
+                            TextStyle = new SCITextFormattingStyle {FontSize = 42 }
+                        },
+                        CoordinateMode = SCIAnnotationCoordinateMode.Relative,
+                        HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Center,
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Center
+                    },
+
+                    // Text annotations
+                    new SCITextAnnotation
+                    {
+                        X1Value = 0.3,
+                        Y1Value = 9.7,
+                        Style = new SCITextAnnotationStyle
+                        {
+                            TextColor = UIColor.White,
+                            TextStyle = new SCITextFormattingStyle { FontSize = 24 },
+                        },
+                        Text = "Annotations are Easy!",
+                    },
+                    new SCITextAnnotation
+                    {
+                        X1Value = 1.9,
+                        Y1Value = 9.0,
+                        Style = new SCITextAnnotationStyle
+                        {
+                            TextColor = UIColor.White,
+                            TextStyle = new SCITextFormattingStyle { FontSize = 10 },
+                        },
+                        Text = "You can create text",
+                    },
+
+                    // Text with Anchor Points
+                    new SCITextAnnotation
+                    {
+                        X1Value = 5,
+                        Y1Value = 8,
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Center,
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Bottom,
+                        Text = "Anchor Center (X1, Y1)",
+                    },
+                    new SCITextAnnotation
+                    {
+                        X1Value = 5,
+                        Y1Value = 8,
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Right,
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Top,
+                        Text = "Anchor Right",
+                    },
+                    new SCITextAnnotation
+                    {
+                        X1Value = 5,
+                        Y1Value = 8,
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        HorizontalAnchorPoint = SCIHorizontalAnchorPoint.Left,
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Top,
+                        Text = "or Anchor Left",
+                    },
+
+                    // Line and TODO LineArrow annotaiton
+                    new SCITextAnnotation
+                    {
+                        X1Value = 0.3,
+                        Y1Value = 6.1,
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Bottom,
+                        Text = "Draw Lines with \nor without arrows",
+                    },
+                    new SCILineAnnotation
+                    {
+                        X1Value = 1.0,
+                        Y1Value = 4.0,
+                        X2Value = 2.0,
+                        Y2Value = 6.0,
+                        Style = new SCILineAnnotationStyle { LinePen = new SCISolidPenStyle(0xFF555555, 2f) }
+                    },
+                    new SCILineAnnotation
+                    {
+                        X1Value = 1.2,
+                        Y1Value = 3.8,
+                        X2Value = 2.5,
+                        Y2Value = 6.0,
+                        Style = new SCILineAnnotationStyle {LinePen = new SCISolidPenStyle(0xFF555555, 2f) }
+                    },
+
+                    // Boxes
+                    new SCITextAnnotation
+                    {
+                        X1Value = 3.5,
+                        Y1Value = 6.1,
+                        Text = "Draw Boxes",
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Bottom
+                    },
+                    new SCIBoxAnnotation
+                    {
+                        X1Value = 3.5,
+                        Y1Value = 4.0,
+                        X2Value = 5.0,
+                        Y2Value = 5.0,
+                        Style = new SCIBoxAnnotationStyle
+                        {
+                            BorderPen = new SCISolidPenStyle(0xFF279B27, 1f),
+                            FillBrush = new SCILinearGradientBrushStyle(0x550000FF, 0x55FFFF00, SCILinearGradientDirection.Horizontal)
+                        }
+                    },
+                    new SCIBoxAnnotation
+                    {
+                        X1Value = 4.0,
+                        Y1Value = 4.5,
+                        X2Value = 5.5,
+                        Y2Value = 5.5,
+                        Style = new SCIBoxAnnotationStyle
+                        {
+                            BorderPen = new SCISolidPenStyle(0xFFFF1919, 1f),
+                            FillBrush = new SCISolidBrushStyle(0x55FF1919)
+                        }
+                    },
+                    new SCIBoxAnnotation
+                    {
+                        X1Value = 4.5,
+                        Y1Value = 5.0,
+                        X2Value = 6.0,
+                        Y2Value = 6.0,
+                        Style = new SCIBoxAnnotationStyle
+                        {
+                            BorderPen = new SCISolidPenStyle(0xFF279B27, 1f),
+                            FillBrush = new SCISolidBrushStyle(0x55279B27)
+                        }
+                    },
+
+                    // Custom chapes
+                    new SCITextAnnotation
+                    {
+                        X1Value = 7.0,
+                        Y1Value = 6.1,
+                        Style = new SCITextAnnotationStyle { TextColor = UIColor.White },
+                        VerticalAnchorPoint = SCIVerticalAnchorPoint.Bottom,
+                        Text = "Or Custom Shapes"
+                    },
+                    new SCICustomAnnotation
+                    {
+                        CustomView = new UIImageView(UIImage.FromBundle("GreenArrow")),
+                        X1Value = 8,
+                        Y1Value = 5.5
+                    },
+                    new SCICustomAnnotation
+                    {
+                        CustomView = new UIImageView(UIImage.FromBundle("RedArrow")),
+                        X1Value = 7.5,
+                        Y1Value = 5
+                    },
+
+                    // Horizontal Lines
+                    horizontalLine1,
+                    horizontalLine2,
+
+                    // Vertical Lines
+                    verticalLine1,
+                    verticalLine2
+                };
+
+                Surface.ChartModifiers = new SCIChartModifierCollection
+                {
+                    new SCIZoomPanModifier(),
+                    new SCIPinchZoomModifier(),
+                    new SCIZoomExtentsModifier()
+                };
+            }
+        }
+    }
+}
