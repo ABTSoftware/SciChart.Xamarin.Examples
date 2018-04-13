dfbc995701a9eb6312d1f847851a0f7870957622
SciChart DevTeam <devteam@scichart.com>
2018-04-13 18:20:48 +0000

    SciChart version 2.1.0.691

---
+++ src/Xamarin.Showcase.Demo/scichartshowcase/scichartshowcasePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace scichartshowcase
+{
+    public partial class scichartshowcasePage : ContentPage
+    {
+        public scichartshowcasePage()
+        {
+            InitializeComponent();
+
+            LogoImage.Source = ImageSource.FromFile("scichart-logo-ret1.png");
+            BannerImage.Source = ImageSource.FromFile("Banner.png");
+
+            SkipButton.Clicked += (sender, e) =>
+            {
+                Navigation.PushAsync(new ShowcaseListPage.ShowcaseListPage());
+                Navigation.RemovePage(this);
+            };
+        }
+    }
+}
