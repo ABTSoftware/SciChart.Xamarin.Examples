4c3097e7fb7ba553207f30468ec2f4633b2896d5
SciChart DevTeam <devteam@scichart.com>
2020-04-08 17:11:07 +0100

    SciChart Xamarin.Android Examples v3.1.0.4259

---
+++ src/Xamarin.Examples.Demo.Droid/Fragments/Examples/RoundedColumnsSeries/RoundedColumnsChartFragment.cs
@@ -0,0 +1,56 @@
+﻿using Android.Views.Animations;
+using SciChart.Charting.Model;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Charting.Modifiers;
+using SciChart.Charting.Visuals;
+using SciChart.Charting.Visuals.Animations;
+using SciChart.Charting.Visuals.Axes;
+using SciChart.Data.Model;
+using SciChart.Drawing.Common;
+using Xamarin.Examples.Demo.Droid.Fragments.Base;
+
+namespace Xamarin.Examples.Demo.Droid.Fragments.Examples
+{
+    [ExampleDefinition("Custom Rounded Columns Chart", description: "Generates a rounded columns chart in code.", icon: ExampleIcon.ColumnChart)]
+    public class RoundedColumnsChartFragment : ExampleBaseFragment
+    {
+        public SciChartSurface Surface => View.FindViewById<SciChartSurface>(Resource.Id.chart);
+
+        public override int ExampleLayoutId => Resource.Layout.Example_Single_Chart_Fragment;
+
+        protected override void InitExample()
+        {
+            var xAxis = new NumericAxis(Activity) { GrowBy = new DoubleRange(0.1, 0.1) };
+            var yAxis = new NumericAxis(Activity) { GrowBy = new DoubleRange(0.2, 0.2) };
+
+            var dataSeries = new XyDataSeries<double, double>();
+
+            var yValues = new[] { 50, 35, 61, 58, 50, 50, 40, 53, 55, 23, 45, 12, 59, 60 };
+            for (var i = 0; i < yValues.Length; i++)
+            {
+                dataSeries.Append(i, yValues[i]);
+            }
+
+            var rSeries = new RoundedColumnsRenderableSeries
+            {
+                DataSeries = dataSeries,
+                FillBrushStyle = new SolidBrushStyle(0xFF3CF3A6),
+            };
+
+            using (Surface.SuspendUpdates())
+            {
+                Surface.XAxes.Add(xAxis);
+                Surface.YAxes.Add(yAxis);
+                Surface.RenderableSeries.Add(rSeries);
+                Surface.ChartModifiers = new ChartModifierCollection
+                {
+                    new ZoomPanModifier(),
+                    new PinchZoomModifier(),
+                    new ZoomExtentsModifier(),
+                };
+
+                new ScaleAnimatorBuilder(rSeries) { Interpolator = new OvershootInterpolator(), Duration = 1500, StartDelay = 350 }.Start();
+            }
+        }
+    }
+}
---
+++ src/Xamarin.Examples.Demo.Droid/Fragments/Examples/RoundedColumnsSeries/RoundedColumnsRenderableSeries.cs
@@ -0,0 +1,73 @@
+﻿using SciChart.Charting.Visuals.RenderableSeries;
+using SciChart.Charting.Visuals.RenderableSeries.Data;
+using SciChart.Charting.Visuals.RenderableSeries.HitTest;
+using SciChart.Core.Model;
+using SciChart.Drawing.Common;
+
+namespace Xamarin.Examples.Demo.Droid
+{
+    public class RoundedColumnsRenderableSeries: FastColumnRenderableSeries
+    {
+        private FloatValues topEllipsesBuffer = new FloatValues();
+        private FloatValues rectsBuffer = new FloatValues();
+        private FloatValues bottomEllipsesBuffer = new FloatValues();
+
+        public RoundedColumnsRenderableSeries() : base(new ColumnRenderPassData(), new ColumnHitProvider(), new NearestColumnPointProvider())
+        {
+        }
+
+        protected override void DisposeCachedData()
+        {
+            base.DisposeCachedData();
+
+            topEllipsesBuffer.DisposeItems();
+            rectsBuffer.DisposeItems();
+            bottomEllipsesBuffer.DisposeItems();
+        }
+
+        protected override void InternalDraw(IRenderContext2D renderContext, IAssetManager2D assetManager, ISeriesRenderPassData renderPassData)
+        {
+            // Don't draw transparent series
+            if (Opacity == 0) return;
+
+            if (FillBrushStyle == null || !FillBrushStyle.IsVisible) return;
+
+            ColumnRenderPassData rpd = (ColumnRenderPassData)renderPassData;
+            float diameter = rpd.ColumnPixelWidth;
+            updateDrawingBuffers(rpd, diameter, rpd.ZeroLineCoord);
+
+            IBrush2D brush = assetManager.CreateBrush(FillBrushStyle);
+            renderContext.FillRects(rectsBuffer.GetItemsArray(), 0, rectsBuffer.Size(), brush);
+            renderContext.DrawEllipses(topEllipsesBuffer.GetItemsArray(), 0, topEllipsesBuffer.Size(), diameter, diameter, brush);
+            renderContext.DrawEllipses(bottomEllipsesBuffer.GetItemsArray(), 0, bottomEllipsesBuffer.Size(), diameter, diameter, brush);
+        }
+
+        void updateDrawingBuffers(ColumnRenderPassData renderPassData, float columnPixelWidth, float zeroLine)
+        {
+            float halfWidth = columnPixelWidth / 2;
+
+            topEllipsesBuffer.SetSize(renderPassData.PointsCount() * 2);
+            rectsBuffer.SetSize(renderPassData.PointsCount() * 4);
+            bottomEllipsesBuffer.SetSize(renderPassData.PointsCount() * 2);
+
+            float[] xCoordsArray = renderPassData.XCoords.GetItemsArray();
+            float[] yCoordsArray = renderPassData.YCoords.GetItemsArray();
+            for (int i = 0, count = renderPassData.PointsCount(); i < count; i++)
+            {
+                float x = xCoordsArray[i];
+                float y = yCoordsArray[i];
+
+                topEllipsesBuffer.Set(i * 2, x);
+                topEllipsesBuffer.Set(i * 2 + 1, y - halfWidth);
+
+                rectsBuffer.Set(i * 4, x - halfWidth);
+                rectsBuffer.Set(i * 4 + 1, y - halfWidth);
+                rectsBuffer.Set(i * 4 + 2, x + halfWidth);
+                rectsBuffer.Set(i * 4 + 3, zeroLine + halfWidth);
+
+                bottomEllipsesBuffer.Set(i * 2, x);
+                bottomEllipsesBuffer.Set(i * 2 + 1, zeroLine + halfWidth);
+            }
+        }
+    }
+}
