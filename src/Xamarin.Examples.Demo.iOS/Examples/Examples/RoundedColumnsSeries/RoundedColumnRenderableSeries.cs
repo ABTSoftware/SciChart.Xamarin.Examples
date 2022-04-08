935273a7ece26f177f39ac8e310ac55d6dd4d09c
SciChart DevTeam <devteam@scichart.com>
2022-04-08 08:14:48 +0100

    SciChart Xamarin.iOS Examples v4.4.0.5067

--- src/Xamarin.Examples.Demo.iOS/Examples/Examples/RoundedColumnsSeries/RoundedColumnRenderableSeries.cs
+++ src/Xamarin.Examples.Demo.iOS/Examples/Examples/RoundedColumnsSeries/RoundedColumnRenderableSeries.cs
@@ -5,9 +5,8 @@ namespace Xamarin.Examples.Demo.iOS
 {
     public class RoundedColumnRenderableSeries: SCIFastColumnRenderableSeries
     {
-        private readonly SCIFloatValues topEllipsesBuffer = new SCIFloatValues();
+        private const double cornerRadius = 20.0;
         private readonly SCIFloatValues rectsBuffer = new SCIFloatValues();
-        private readonly SCIFloatValues bottomEllipsesBuffer = new SCIFloatValues();
 
         public RoundedColumnRenderableSeries() : base(new SCIColumnRenderPassData(), new SCIColumnHitProvider(), new SCINearestColumnPointProvider())
         {
@@ -17,9 +16,7 @@ namespace Xamarin.Examples.Demo.iOS
         {
             base.DisposeCachedData();
 
-            topEllipsesBuffer.Dispose();
             rectsBuffer.Dispose();
-            bottomEllipsesBuffer.Dispose();
         }
 
         protected override void InternalDraw(IISCIRenderContext2D renderContext, IISCIAssetManager2D assetManager, IISCISeriesRenderPassData renderPassData)
@@ -34,38 +31,24 @@ namespace Xamarin.Examples.Demo.iOS
             updateDrawingBuffersWithData(rpd, rpd.ColumnPixelWidth, rpd.ZeroLineCoord);
 
             IISCIBrush2D brush = assetManager.BrushWithStyle(fillStyle);
-            renderContext.FillRects(rectsBuffer.ItemsArray, 0, rectsBuffer.Count, brush);
-            renderContext.FillEllipses(topEllipsesBuffer.ItemsArray, 0, topEllipsesBuffer.Count, brush);
-            renderContext.FillEllipses(bottomEllipsesBuffer.ItemsArray, 0, bottomEllipsesBuffer.Count, brush);
+            renderContext.DrawRoundedRects(rectsBuffer.ItemsArray, 0, rectsBuffer.Count, null, brush, cornerRadius, cornerRadius);
         }
 
         void updateDrawingBuffersWithData(SCIColumnRenderPassData renderPassData, float columnPixelWidth, float zeroLine)
         {
             float halfWidth = columnPixelWidth / 2;
 
-            topEllipsesBuffer.Count = renderPassData.PointsCount * 4;
             rectsBuffer.Count = renderPassData.PointsCount * 4;
-            bottomEllipsesBuffer.Count = renderPassData.PointsCount * 4;
 
             for (var i = 0; i < renderPassData.PointsCount; i++)
             {
                 float x = renderPassData.XCoords.GetValueAt(i);
                 float y = renderPassData.YCoords.GetValueAt(i);
 
-                topEllipsesBuffer.Set(x - halfWidth, i * 4);
-                topEllipsesBuffer.Set(y, i * 4 + 1);
-                topEllipsesBuffer.Set(x + halfWidth, i * 4 + 2);
-                topEllipsesBuffer.Set(y - columnPixelWidth, i * 4 + 3);
-
                 rectsBuffer.Set(x - halfWidth, i * 4);
                 rectsBuffer.Set(y - halfWidth, i * 4 + 1);
                 rectsBuffer.Set(x + halfWidth, i * 4 + 2);
                 rectsBuffer.Set(zeroLine + halfWidth, i * 4 + 3);
-
-                bottomEllipsesBuffer.Set(x - halfWidth, i * 4);
-                bottomEllipsesBuffer.Set(zeroLine + columnPixelWidth, i * 4 + 1);
-                bottomEllipsesBuffer.Set(x + halfWidth, i * 4 + 2);
-                bottomEllipsesBuffer.Set(zeroLine, i * 4 + 3);
             }
         }
     }
